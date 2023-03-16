a9ad83b52da7d0b05a4c1c4b4a393defed5a8d59
SaiCharan20012007 <saicharan20012007@gmail.com>
2023-03-16 03:28:50 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import TopSection from "./components/TopSection";
+import DigitalMarketingAgency from "./components/DigitalMarketingAgency";
+import UGCompany from "./components/UGCompany";
+import DigitalMarketingAgencyLondon from "./components/DigitalMarketingAgencyLondon";
+import HighQualityDigitalGrowthSolutions from "./components/HighQualityDigitalGrowthSolutions";
+import CountSection from "./components/CountSection";
+import ServicesCards from "./components/ServicesCards";
+import BottomSection from "./components/BottomSection";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <>
+    <TopSection />
+    <DigitalMarketingAgency />
+    <UGCompany />
+    <DigitalMarketingAgencyLondon />
+    <HighQualityDigitalGrowthSolutions />
+    <CountSection />
+    <ServicesCards />
+    <BottomSection />
+  </>
+);
 
 export default App;
